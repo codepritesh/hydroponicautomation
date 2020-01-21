bda3cfa7c8f104913b85a4ac12e657fc372e0a19
Cooding PHP <pmpmaharana97@gmail.com>
2020-01-21 16:44:09 +0530

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "sriganesh"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "RadhaRadha"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 696386     // replace 0000000 with your channel numberof thingspeak
+#define SECRET_WRITE_APIKEY "DIMU7I89KRYUG7I5"   // replace XYZ with your channel write API Key (thingspeak)
